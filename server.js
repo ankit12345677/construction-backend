15848a82f46a6dc3e762d7b35b9720f81c59ac49
Ankit Choube <ankit.choube.16236@ves.ac.in>
2025-09-28 17:26:40 +0530

    Updated server.js with Gmail SMTP / contact form

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.static('public'));
 // Email transporter configuration
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for port 465
+  port: 587,
+  secure: false, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
