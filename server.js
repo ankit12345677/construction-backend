e4d8b510b69f4882934aa866f943950247067f39
Ankit Choube <ankit.choube.16236@ves.ac.in>
2025-09-30 02:23:44 +0530

    Updated server.js for Google credentials environment variable

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const auth = new google.auth.GoogleAuth({
   credentials: { 
     type: process.env.GOOGLE_TYPE,
     project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key:  process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n'),
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
     auth_uri: process.env.GOOGLE_AUTH_URI,
